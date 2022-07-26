acabb96bf6d72a1774a6ea297e058bb4dcf30791
qiang <qiang.lilq@shopee.com>
2022-07-26 15:54:05 +0800

    backup

--- mybin/git_commit.sh
+++ mybin/git_commit.sh
@@ -4,3 +4,6 @@ echo "commit obsidian ok"
 
 cd /Users/qiang.lilq/Documents/backup;git add . ; git commit -m 'backup'; git push origin main
 echo "commit backup ok"
+
+cd /Users/qiang.lilq/code/test/gallery; git add . ; git commit -m 'automic save data'; git push origin main
+echo "commit gallery ok" 
